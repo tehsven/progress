7e30582020c7167f8a22d11cc369c7f5d52f0bdc
tehsven <tehsven@gmail.com>
2016-06-16 09:28:57 -0700

    First commit

---
+++ lib/progress.rb
@@ -0,0 +1,50 @@
+class Progress
+
+  attr_reader :total, :yield_interval, :yield_block, :current, :start_time, :end_time
+
+  def initialize(total:, current: 0, yield_interval:, yield_block:)
+    @total = total
+    @current = current
+    @yield_interval = yield_interval
+    @yield_block = yield_block
+  end
+
+  def update(delta)
+    @start_time ||= Time.now
+    @current += delta
+    yield_block.call(results) if should_yield
+  end
+
+  def should_yield
+    current % yield_interval == 0
+  end
+
+  def results
+    {
+      current: current,
+      total: total,
+      percent_complete: percent_complete,
+      ops_per_sec: ops_per_sec,
+      eta: eta
+    }
+  end
+
+  def percent_complete
+    100 * (current.to_f/total.to_f)
+  end
+
+  def ops_per_sec
+    current / (Time.now - start_time)
+  end
+
+  def eta
+    remaining = total - current
+    if remaining <= 0
+      @end_time ||= Time.now
+      end_time
+    else
+      Time.at(Time.now + (ops_per_sec / remaining))
+    end
+  end
+
+end
---
+++ progress.gemspec
@@ -0,0 +1,22 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+Gem::Specification.new do |spec|
+  spec.name          = "progress"
+  spec.version       = "0.1.0"
+  spec.authors       = ["tehsven"]
+  spec.email         = ["tehsven@gmail.com"]
+
+  spec.summary       = %q{For tracking progress}
+  spec.description   = %q{A gem for tracking progress of long running tasks}
+  spec.homepage      = "https://github.com/tehsven/progress"
+
+  spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
+  spec.bindir        = "exe"
+  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  spec.require_paths = ["lib"]
+
+  spec.add_development_dependency "bundler", "~> 1.11"
+  spec.add_development_dependency "rake", "~> 10.0"
+end
